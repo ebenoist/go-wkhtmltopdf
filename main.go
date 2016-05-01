d971ec851948635f7ec72787ea382c90f95dc526
Erik Benoist <ebenoist@gmail.com>
2016-05-01 14:48:50 -0500

    ?

--- main.go
+++ main.go
@@ -9,16 +9,32 @@ package main
 import "C"
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"os/exec"
 	"unsafe"
 )
 
+const WKHTMLTOPDF_CMD = "wkhtmltopdf"
+
 func main() {
 	var html string
+	var cbinding bool
+
+	flag.BoolVar(&cbinding, "cbinding", false, "Perform with c binding")
 	flag.StringVar(&html, "html", "<b>Hello, World</b>", "HTML to convert to PDF")
 	flag.Parse()
 
+	if cbinding {
+		convertWithBindings(html)
+	} else {
+		HTMLToPDF(html)
+	}
+}
+
+func convertWithBindings(html string) {
+	fmt.Printf("with C")
 	C.wkhtmltopdf_init(C.false)
 	globalSettings := C.wkhtmltopdf_create_global_settings()
 	objectSettings := C.wkhtmltopdf_create_object_settings()
@@ -32,5 +48,25 @@ func main() {
 
 	pdf := C.GoStringN((*C.char)(unsafe.Pointer(*outputBuffer)), C.int(length))
 
-	fmt.Print("%s", pdf)
+	fmt.Printf("%s", pdf)
+}
+
+func HTMLToPDF(html string) {
+	fmt.Printf("with shell")
+	cmd := exec.Command(WKHTMLTOPDF_CMD, "-", "-")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	writer, _ := cmd.StdinPipe()
+
+	// Write to the buffer asynchronously so that it does not hang on large payloads
+	go func() {
+		writer.Write([]byte(html))
+		writer.Close()
+	}()
+
+	cmd.Run()
+
+	fmt.Printf("%s", out.Bytes())
 }
